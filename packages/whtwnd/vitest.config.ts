d3cea4599b7d4b5c7dbb0f19515695a0efd46693
mkizka <g.mkizka@gmail.com>
2025-11-17 22:21:07 +0900

    一旦テスト削除

--- packages/whtwnd/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vitest/config";
-
-export default defineConfig({
-  test: {
-    clearMocks: true,
-    exclude: ["**/node_modules/**", "**/dist/**"],
-    passWithNoTests: true,
-  },
-});
